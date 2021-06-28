320467046823704001e8970d40d505b4f2a5006e
Bharat Dave <davebharat93@gmail.com>
2021-06-28 15:41:22 -0400

    Render basic bootstrap navbar and custom searchfield component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import Searchfield from "./components/Searchfield";
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <NavBar />
       <Searchfield />
-      <EmployeeTable />
     </div>
   );
 }
---
+++ src/components/Searchfield.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Searchfield = () => {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Searchfield;
