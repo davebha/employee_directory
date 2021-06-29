cba22b53747700ea594d13002817f4971de30ee1
Bharat Dave <davebharat93@gmail.com>
2021-06-28 21:30:12 -0400

    Render basic EmployeeTable component layout

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
 import Searchfield from "./components/Searchfield";
+import EmployeeTable from "./components/EmployeeTable";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Searchfield />
+      <EmployeeTable />
     </div>
   );
 }
