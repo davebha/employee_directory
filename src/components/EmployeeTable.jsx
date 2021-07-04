ce786bc88905a1fc2628116dee9e651d377c5a8a
davebha <davebharat93@gmail.com>
2021-07-04 18:58:44 -0400

    Add all the Navbar modules from react-bootstrap and change headers to include employee Name,DoB,E-mail and Phone Number for the table

--- src/components/EmployeeTable.jsx
+++ src/components/EmployeeTable.jsx
@@ -6,28 +6,28 @@ const EmployeeTable = () => {
     <Table responsive>
       <thead>
         <tr>
-          <th>#</th>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <th key={index}>Table heading</th>
-          ))}
+          <th>Name</th>
+          <th>E-mail</th>
+          <th>Phone Number</th>
+          <th>Date of Birth</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>1</td>
-          {Array.from({ length: 12 }).map((_, index) => (
+          {Array.from({ length: 3 }).map((_, index) => (
             <td key={index}>Table cell {index}</td>
           ))}
         </tr>
         <tr>
           <td>2</td>
-          {Array.from({ length: 12 }).map((_, index) => (
+          {Array.from({ length: 3 }).map((_, index) => (
             <td key={index}>Table cell {index}</td>
           ))}
         </tr>
         <tr>
           <td>3</td>
-          {Array.from({ length: 12 }).map((_, index) => (
+          {Array.from({ length: 3 }).map((_, index) => (
             <td key={index}>Table cell {index}</td>
           ))}
         </tr>
