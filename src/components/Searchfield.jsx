4dfba054dcca3d97071504326128fb97691ebce5
davebha <davebharat93@gmail.com>
2021-07-11 22:20:47 -0400

    Display the searchfield input

--- src/components/Searchfield.jsx
+++ src/components/Searchfield.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-const Searchfield = () => {
+const Searchfield = ({ employeeList }) => {
+  let [query, setQuery] = useState("");
+
+  /*
+  useEffect(() => {
+    checkQuery(employeeList);
+  }, [query]);
+  
+  const checkQuery = employeeList => {
+    let name = JSON.stringify(
+      employeeList.map(e => {
+        e.name.first;
+      })
+    );
+    console.log(name);
+  };*/
+
+  const searchChange = event => {
+    setQuery(event.target.value);
+    console.log(query);
+  };
   return (
     <div>
-      <input type="text" />
+      <input type="text" onChange={searchChange} />
     </div>
   );
 };
